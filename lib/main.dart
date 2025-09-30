d4e60a87651adfc1a66a7865411089a52577180a
AD99i <zawarudo74u@gmail.com>
2025-09-30 16:59:36 +0200

    m

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,20 @@ class ProjectManagerApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  final List<Project> _projects = [
-    Project(title: 'Projet Mannhattan', desc: 'un projet vraiment énorme'),
-    Project(title: 'Projet important', desc: 'un projet très important'),
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentPageIndex = 0;
+
+  final List<Widget> _pages = [
+    ProjectListView(),
+    ContributionPage(),
   ];
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -45,31 +54,14 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         leading: const Icon(Icons.rocket_launch_rounded),
       ),
-      body: ListView.builder(
-        itemCount: _projects.length,
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, index) {
-          final project = _projects[index];
-
-          return Card(
-            margin: const EdgeInsets.symmetric(vertical: 8.0),
-            elevation: 2,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: ListTile(
-              leading: const Icon(Icons.work_outline, color: Colors.indigo),
-              title: Text(project.title),
-              subtitle: Text(project.desc),
-              trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-              onTap: () {},
-            ),
-          );
-        },
-      ),
+      body: _pages[currentPageIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        onTap: null,
+
+        onTap: (int index){
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.folder_open),
@@ -83,6 +75,49 @@ class HomeScreen extends StatelessWidget {
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.grey,
       ),
+    );
+  }
+}
+
+class ContributionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("page de contribution"),
+    );
+  }
+
+}
+
+class ProjectListView extends StatelessWidget {
+  final List<Project> _projects = [
+    Project(title: 'Projet 1', desc: 'projet 1'),
+    Project(title: 'Projet 2', desc: 'projet 2'),
+    Project(title: 'Projet 3', desc: 'projet 3'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: _projects.length,
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, index) {
+        final project = _projects[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(vertical: 8.0),
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: ListTile(
+            leading: Icon(Icons.work_outline, color: Theme.of(context).colorScheme.primary),
+            title: Text(project.title),
+            subtitle: Text(project.desc),
+            trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+            onTap: () {},
+          ),
+        );
+      },
     );
   }
 }
